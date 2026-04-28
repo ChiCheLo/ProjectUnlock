5b1180a63b784f4ec1a43c3623d153d296f53ede
30percentoff <s9617129172@gmail.com>
2026-04-28 13:37:49 +0800

    login不會有線索交易

--- sevenzhe/front-end/App.vue
+++ sevenzhe/front-end/App.vue
@@ -1,8 +1,11 @@
 <template>
   <router-view />
-  <ClueTrade />
+  <ClueTrade v-if="route.name !== 'login'" />
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
 import ClueTrade from './src/components/ClueTrade.vue'
+
+const route = useRoute()
 </script>
